8755b024a6183d09ef3ea0234102fa9be62b22ef
Scott Atkins <satkins16@gmail.com>
2017-11-30 13:15:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 $(document).ready(function (){
 });
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  $.get('https://api.github.com/search/repositories?q=${searchTerms}', data => {
+    const template = Handlebars.compile($('#results-template').html())
+    $('#results').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
